8f34c1eed620d3da75a0602836cd7ab27665fc40
David Négrier <david@davidtcm>
2012-11-15 21:20:02 +0100

    Adding installer

---
+++ src/install.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * Copyright (c) 2012 David Negrier
+ * 
+ * See the file LICENSE.txt for copying permission.
+ */
+
+require_once __DIR__."/../../autoload.php";
+
+use Mouf\Actions\InstallUtils;
+use Mouf\MoufManager;
+
+// Let's init Mouf
+InstallUtils::init(InstallUtils::$INIT_APP);
+
+// Let's create the instance
+$moufManager = MoufManager::getMoufManager();
+if (!$moufManager->instanceExists("errorLogLogger")) {
+	
+	$errorLogLogger = $moufManager->createInstance("ErrorLogLogger");
+	// Let's set a name for this instance (otherwise, it would be anonymous)
+	$errorLogLogger->setName("errorLogLogger");
+	$errorLogLogger->getProperty("level")->setValue(4);
+	/*$moufManager->declareComponent("errorLogLogger", "ErrorLogLogger");
+	$moufManager->setParameter("errorLogLogger", "level", 4);*/
+}
+
+// Let's rewrite the MoufComponents.php file to save the component
+$moufManager->rewriteMouf();
+
+// Finally, let's continue the install
+InstallUtils::continueInstall();
+?>
